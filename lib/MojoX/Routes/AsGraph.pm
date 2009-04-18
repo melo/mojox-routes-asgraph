ac7bd768b1a191c79e2eb6eabe58efc53b91aff0
Pedro Melo <melo@simplicidade.org>
2009-04-18 15:36:43 +0100

    Initial skel, generated with module-starter
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ lib/MojoX/Routes/AsGraph.pm
@@ -0,0 +1,107 @@
+package MojoX::Routes::AsGraph;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+MojoX::Routes::AsGraph - The great new MojoX::Routes::AsGraph!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use MojoX::Routes::AsGraph;
+
+    my $foo = MojoX::Routes::AsGraph->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Pedro Melo, C<< <melo at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-mojox-routes-asgraph at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MojoX-Routes-AsGraph>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc MojoX::Routes::AsGraph
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MojoX-Routes-AsGraph>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/MojoX-Routes-AsGraph>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/MojoX-Routes-AsGraph>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/MojoX-Routes-AsGraph/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Pedro Melo, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of MojoX::Routes::AsGraph
